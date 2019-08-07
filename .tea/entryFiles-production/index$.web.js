abe7f6299c27256918a18f5d269555d05e338e70
WIN-Q53DLURAP0O\Administrator <826564596@qq.com>
2019-08-07 09:38:11 +0800

    外出删除按钮

--- .tea/entryFiles-production/index$.web.js
+++ .tea/entryFiles-production/index$.web.js
@@ -1,5 +1,6 @@
 require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
+
 require('../../util/table/table');
 require('../../util/table/column/column');
 require('../../node_modules/mini-ddui/es/list/index');
@@ -44,3 +45,4 @@ require('../../page/start/pickingManage/pickingManage');
 require('../../page/approveDetail/maintain/maintain');
 require('../../page/start/techonologySupply/techonologySupply');
 require('../../page/start/overTime/overTime');
+require('../../page/approveDetail/projectClosure/projectClosure');
